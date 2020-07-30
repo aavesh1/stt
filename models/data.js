0db056829329e9ebbd78ef7b3e07204373f826d9
aavesh1 <aaveshd.ic.17@nsit.net.in>
2020-07-31 03:14:16 +0530

    commit

--- models/data.js
+++ models/data.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose')
 const { model } = require('./user')
 var DataSchema = new mongoose.Schema({
 
-user:String  ,  date : String , topic : String , time : Number 
+    user: String, date: String, topic: String, time: Number , totaltime : Number
 
 })
 
-module.exports = mongoose.model("Data" , DataSchema)
+module.exports = mongoose.model("Data", DataSchema)
 
