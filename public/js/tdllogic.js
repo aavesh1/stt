e82d769751a744eca3eef66afe0e090aaad1fd1a
aavesh1 <aaveshd.ic.17@nsit.net.in>
2020-07-31 00:45:18 +0530

    commit

--- public/js/tdllogic.js
+++ public/js/tdllogic.js
@@ -1,6 +1,6 @@
-$("ul").on("click", "li", function () {
+/*$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
-});
+});*/
 
 $(".fa-plus").click(function () {
   $("input").fadeToggle(20);
