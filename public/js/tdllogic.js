120787249445273e6029c4a6a976d9cb14558276
aavesh1 <aaveshd.ic.17@nsit.net.in>
2020-08-06 05:21:35 +0530

    commit

--- public/js/tdllogic.js
+++ public/js/tdllogic.js
@@ -7,7 +7,7 @@ $(".fa-plus").click(function () {
 });
 
 // Setting Date field value to today.
-
+$(document).ready(function(){
 var field = document.querySelector("#today");
 var date = new Date();
 field.value =
@@ -17,14 +17,14 @@ field.value =
   "-" +
   date.getDate().toString().padStart(2, 0);
 
-  
-var field2 = document.querySelector("#time");
-var date2 = new Date();
-field2.value =
-  date2.getFullYear().toString() +
-  "-" +
-  (date2.getMonth() + 1).toString().padStart(2, 0) +
-  "-" + date2.getDate().toString().padStart(2, 0) + ' at ' + date2.getHours().toString().padStart(2,0) + ':' + date2.getMinutes().toString().padStart(2,0);
 
+var date2 = new Date();
+  var value = date2.getFullYear().toString() +
+    "-" + (date2.getMonth() + 1).toString().padStart(2, 0) +
+    "-" + date2.getDate().toString().padStart(2, 0) + ' at ' + date2.getHours().toString().padStart(2, 0) + ':' + date2.getMinutes().toString().padStart(2, 0);
+$('input[name=time]').val(value); 
+console.log(value)
+
+})
 
 
